281407c76d06a028cca3ab4476f01a21b8c7656d
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-05-05 14:20:57 +0200

    favorite button + MAYBE local storage

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// get favorites from local storage or empty array
+var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+// add class 'fav' to each favorite
+favorites.forEach(function (favorite) {
+    document.getElementById(favorite).className = "fav";
+});
+// register click event listener
+document.querySelector(".list").addEventListener("click", function (e) {
+    var id = e.target.id,
+        item = e.target,
+        index = favorites.indexOf(id);
+    // return if target doesn't have an id (shouldn't happen)
+    if (!id) return;
+    // item is not favorite
+    if (index == -1) {
+        favorites.push(id);
+        item.className = "fav";
+        // item is already favorite
+    } else {
+        favorites.splice(index, 1);
+        item.className = "";
+    }
+    // store array in local storage
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+});
+
+// local storage stores strings so we use JSON to stringify for storage and parse to get out of storage
+
 // display none element transitionned after and of transition
 const logoTransition = document.querySelector(".logo-transition");
 logoTransition.addEventListener("animationend", function () {
@@ -34,14 +62,17 @@ async function searchCities(query) {
             );
             cityButtons.forEach((button) => {
                 button.addEventListener("click", async () => {
-
                     // changement de header avec une animation
                     // au clic ajouter l'animation à header--landing
                     // header.
 
-                    header.classList.remove('header--landing');
-                    document.querySelector("form").classList.remove('form--landing');
-                    document.querySelector(".section-main").classList.remove('hidden');
+                    header.classList.remove("header--landing");
+                    document
+                        .querySelector("form")
+                        .classList.remove("form--landing");
+                    document
+                        .querySelector(".section-main")
+                        .classList.remove("hidden");
 
                     const location = button.dataset.location;
                     searchInput.value = location;
